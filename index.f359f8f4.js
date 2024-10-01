a489d0720d2babb8a2aa2eb0a0306b14d364aae1
kovaltar <kovaltar@gmail.com>
2024-10-01 18:34:21 +0300

    make build

---
+++ index.f359f8f4.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelectorAll("tr"),l=0;l<e.length;l++)for(var r=e[l],o=1;o<2;o++){var t=r.cells[o].cloneNode(!0);r.insertBefore(t,r.lastElementChild)}
+//# sourceMappingURL=index.f359f8f4.js.map
